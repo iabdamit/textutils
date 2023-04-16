bd6f177fba6b1ebd28d22bdb85cee9552a9f16e1
Amit Bhandari <abdamitbhandari@gmail.com>
2023-04-16 14:25:32 +0530

    y

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,16 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import{
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
   const [mode,setMode]=useState('light')
@@ -41,18 +41,19 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText='About Us'/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Routes>
+    {/* <Routes>
     <Route exact path='/about' element={<About/>}>
+    </Route> */}
+    <TextForm showAlert={showAlert} heading="Enter The Text Here To Analyse" mode={mode}/>
+    {/* <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter The Text Here To Analyse" mode={mode}/>}>
     </Route>
-    <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter The Text Here To Analyse" mode={mode}/>}>
-    </Route>
-    </Routes>
+    </Routes> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </>
     );
   }
